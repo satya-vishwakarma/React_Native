48b579d3be04be4bc0e1e3578fa3401905581fcd
satya-vishwakarma <satya9125@gmail.com>
2024-03-04 10:33:41 +0000

    UI

--- my-native-app/App.tsx
+++ my-native-app/App.tsx
@@ -1,17 +1,32 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet } from "react-native";
+
+
+import { NavigationContainer } from '@react-navigation/native';
 import LoginScreen from "./App/Screens/LogingScreen/Login";
+import HomeScreen from "./App/Screens/HomeScreen/Home";
+import SettingScreen from "./App/Screens/SettingsScreen/Settings";
+import ProfileScreen from "./App/Screens/ProfileScreen/Profile";
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+
+
+
+const Tab = createBottomTabNavigator();
+
 
 export default function App() {
 
-
-
-  
   return (
-
-  <View style ={ styles.container}>
-    <LoginScreen/>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+     
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Screen name="Settings" component={SettingScreen} />
+    </Tab.Navigator>
+    </NavigationContainer>
+  
   );
 }
 
