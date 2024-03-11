d0acc21b135482be6810ea05a2cd812ac1c3ae09
Satish Vishwakarma <staya9125@gmail.com>
2024-03-11 23:15:50 +0530

    UI

--- my-native-app/App.tsx
+++ my-native-app/App.tsx
@@ -1,16 +1,39 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
-import LoginScreen from "./App/Screens/LogingScreen/Login";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import BottomTabNavigation from "./navigation/BottomTabNavigation";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    regular: require("./assets/fonts/Poppins-Regular.ttf"),
+    light: require("./assets/fonts/Poppins-Light.ttf"),
+    bold: require("./assets/fonts/Poppins-Bold.ttf"),
+    medium: require("./assets/fonts/Poppins-Medium.ttf"),
+    extrabold: require("E:/Workspace/React_Native/my-native-app/assets/fonts/Poppins-ExtraBold.ttf"),
+  });
 
+  /*  const onLayoutRootView = useCallback(async () => {
+    console.log(fontsLoaded);
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]); */
 
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
 
-  
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-
-  <View style ={ styles.container}>
-    <LoginScreen/>
+    <View style={styles.container}>
+      <Text style={styles.textStyle}> helddd</Text>
+      <BottomTabNavigation />
     </View>
   );
 }
@@ -21,5 +44,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  textStyle: {
+    fontFamily: "extrabold",
+    fontSize: 40,
   },
 });
--- my-native-app/App/Screens/LogingScreen/Login.tsx
+++ my-native-app/App/Screens/LogingScreen/Login.tsx
@@ -1,15 +1,17 @@
-import React from "react"
-import { Text, View } from "react-native"
+import { useFonts } from "expo-font";
+import React from "react";
+import { Text, View } from "react-native";
 
-const LoginScreen = () =>{
-
+const LoginScreen = () => {
+  const [fonstsLoaded] = useFonts({
+    regular: require("./../../../assets/fonts/Poppins/p"),
+  });
 
   return (
     <View>
-    <Text> Login </Text>
+      <Text> Login d</Text>
     </View>
-  )
-}
+  );
+};
 
-
-export default LoginScreen
+export default LoginScreen;
