c11b6888e625a6c29637d930cf9a854e140162d4
satya-vishwakarma <satya9125@gmail.com>
2024-03-12 05:50:10 +0000

    header

---
+++ my-native-app/App/Screens/Common/HeaderRight.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+
+import Ionicons from "react-native-vector-icons/Ionicons";
+type SearchBarComponentProps = {};
+const HeaderRightScreen = () => {
+  return (
+    <>
+      <Ionicons name="person-circle-outline" size={30} color="black" />
+    </>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default HeaderRightScreen;
